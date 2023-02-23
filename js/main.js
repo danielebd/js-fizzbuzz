a2e3a6c40046ce682ce339edd4039a272cc6f2f3
danielebd <danielebalebalestro@gmail.com>
2023-02-23 17:11:47 +0100

    stampa da 1 a 100 di fizz ogni 3 e buzz ogni 5

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 'use strict'
 
+for(let i = 1; i <= 100; i++) {
+    
+    if(i % 3 === 0){
+        console.log('fizz');
+    }
+
+    if(i % 5 === 0){
+        console.log('buzz');
+    }
+
+    if(i % 5 !== 0 && i % 3 !== 0){
+        console.log(i);
+    }
+    
+}
