ee9f300ac0e4fcaf85481ad013182d380f750cb0
danielebd <danielebalebalestro@gmail.com>
2023-02-23 19:01:18 +0100

    bonus 1 e 2 (forse)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict'
 
-for(let i = 1; i <= 100; i++) {
+/* for(let i = 1; i <= 100; i++) {
     
     if(i % 3 === 0){
         console.log('fizz');
@@ -15,3 +15,86 @@ for(let i = 1; i <= 100; i++) {
     }
     
 }
+ */
+/* const container = document.querySelector('div.container');
+
+for (let index = 1; index <= 100; index++) {
+
+    
+
+    if(index % 3 === 0){
+
+        const box = `<div class="box box--${'fizz'}">${'fizz'}</div>`;
+
+        container.innerHTML += box;
+
+        console.log(box);
+    }
+
+    if(index % 5 === 0){
+
+        const box = `<div class="box box--${'buzz'}">${'buzz'}</div>`;
+
+        container.innerHTML += box;
+
+        console.log(box);
+    }
+
+
+    if(index % 3 !== 0 && index % 5 !== 0){
+
+        const box = `<div class="box box--${index}">${index}</div>`;
+
+        console.log(box);
+
+        container.innerHTML += box;
+    }
+    
+
+} */
+
+const container = document.querySelector('div.container');
+
+for (let index = 1; index <= 100; index++) {
+
+    if ( index % 3 === 0) {
+        const box = document.createElement('div');
+        box.append('fizz');
+        container.append(box);
+        box.classList.add('box-fizz');
+        console.log(box);
+    }
+
+    if( index % 5 === 0){
+        const box = document.createElement('div');
+        box.append('buzz');
+        container.append(box);
+        box.classList.add('box-buzz');
+
+        console.log(box);
+    }
+
+    if( index % 15 === 0){
+        const box = document.createElement('div');
+        box.append('fizzbuzz');
+        container.append(box);
+        box.classList.add('box-fizzbuzz');
+
+        console.log(box);
+    }
+
+
+    if (index % 3 !== 0 && index % 5 !== 0) {
+        const box = document.createElement('div');
+        box.append(index);
+        container.append(box);
+        box.classList.add('box-number');
+
+        console.log(box);
+    }
+
+}
+
+
+
+
--- readme.md
+++ readme.md
@@ -3,3 +3,7 @@ includere tre condizionali:
 - ad ogni multiplo di 3 stampare fizz
 - ad ogni multiplo di 5 stampare buzz
 - stampare ogni i che non sia multiplo di 3 o 5 tramite - if(i % 5 !== 0 && i % 3 !== 0){}
+
+Per il bonus usare la nuova proprietà createElement per l'index 3, 5 e 15. Usare lo stesso procedimento sopra citato.
+creare delle classi degli elementi nuovi creati per andare a lavorare css e porre le dovute modifiche per creare una tabella simil tombola.
+
